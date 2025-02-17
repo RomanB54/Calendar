fbae9032a96bd8302564ea5e03419ad6928c6584
RomanB54 <r.barsukov@mgmnt.ru>
2025-02-17 10:51:28 +0300

    init_enviroment

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import jest from 'eslint-plugin-jest';
+import eslintRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslintRecommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'prefer-const': 'warn',
+      'no-unused-vars': 'off',
+      semi: 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+  {
+    files: ['srcjs/*.js'],
+    ...jest.configs['flat/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts'],
+    rules: {
+      'import/no-unresolved': 'off',
+      'import/extensions': ['warn', 'never'],
+    },
+  },
+);
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.ts'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: 'defaults',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
